298ae98a7f74e10fbfced6cb4e15e363536320e4
manaatarod <mana.atarod@gmail.com>
2021-06-13 14:55:22 +0430

    Documentation and comments added

--- Js/slider.js
+++ Js/slider.js
@@ -1,71 +1,63 @@
+/**
+ * creating Slider class for writing JavaScript codes for the slider
+*/
 class Slider {
     constructor({
-        sliderSelector = '#slider',
-        sliderContainerSelector = '.slider-container',
-        previousSelector = '#previous',
-        nextSelector = '#next',
-        transitionTime = 10000,
+        heroSlider = '#slider',
+        imageContainer = '.slider-container',
+        prev = '#previous',
+        next = '#next',
+        sliderIntervalTime = 10000,
     } = {}) {
-        this.slider = document.querySelector(sliderSelector);
-        this.slides = document.querySelectorAll(`${sliderContainerSelector} img`).length;
-        this.sliderContainer = document.querySelector(sliderContainerSelector);
-        this.previousBtn = document.querySelector(previousSelector);
-        this.nextBtn = document.querySelector(nextSelector);
+        this.slider = document.querySelector(heroSlider);
+        this.slides = document.querySelectorAll(`${imageContainer} img`).length;
+        this.sliderContainer = document.querySelector(imageContainer);
+        this.previousBtn = document.querySelector(prev);
+        this.nextBtn = document.querySelector(next);
         this.slideSize = this.slider.offsetWidth;
         this.currentSlide = 0;
         this.setEventListeners();
-        this.setAutoPlay(transitionTime);
+        this.autoPlay(sliderIntervalTime);
     }
 
+    /**
+     * Move the slides one by slide
+     */
     moveSlides() {
         this.sliderContainer.style.transform = `translateX(${this.currentSlide * this.slideSize}px)`;
     };
 
+    /**
+     * Move the slider to the next slide
+     */
     nextSlide() {
         this.currentSlide = this.currentSlide <= 0 ? this.slides - 1 : this.currentSlide - 1;
         this.moveSlides();
     };
 
+    /**
+     * Move the slider to the previous slide
+     */
     previousSlide() {
         this.currentSlide = this.currentSlide >= this.slides - 1 ? 0 : this.currentSlide + 1;
         this.moveSlides();
     };
 
+    /**
+     * Adding event listeners to the sldier control buttons
+     */
     setEventListeners() {
         this.nextBtn.addEventListener('click', this.nextSlide.bind(this));
         this.previousBtn.addEventListener('click', this.previousSlide.bind(this));
     }
 
-    setAutoPlay(transitionTime) {
-        setInterval(this.nextSlide.bind(this), transitionTime);
+    /**
+     * Setting the interval between slides on autoplay
+     * @param {} sliderIntervalTime 
+     */
+    autoPlay(sliderIntervalTime) {
+        setInterval(this.nextSlide.bind(this), sliderIntervalTime);
     }
 }
 
 new Slider();
-/*
-let bgNum = 0
-
-function sliderBackgroundChange() {
-    const images = [
-        'url(images/Tools.png)',
-        'url(images/SchoolClipart.png)',
-        'url(images/Bags.png)',
-    ]
-
-    bgNum = (bgNum + 1) % images.length
-    console.log(bgNum)
-    const currentBg = images[bgNum]
-    document.getElementById('hero-slider').style.backgroundImage = currentBg
-};
-
-function setSliderHeight() {
-    var heroHeigth = document.getElementById('hero-content').height
-    console.log("Hi")
-    console.log(heroHeigth)
-    document.getElementsByClassName('slider').height = heroHeigth
-};
-
-window.onload = function() {
-    setSliderHeight();
-};
-*/
