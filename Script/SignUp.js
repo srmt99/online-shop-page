ca1bfeb88332d81d2c464f989c3a2e221480d2cc
srmt <soroushmortazavi99@gmail.com>
2021-06-14 11:47:52 +0430

    added SignUp validation

---
+++ Script/SignUp.js
@@ -0,0 +1,177 @@
+// setting restrictions on NAME input
+const fname = document.getElementById('name_input');
+const fnameError = document.querySelector('#name_input + span.error');
+
+fname.addEventListener('focus', function(event) {
+  // first, we'll disappear all other input messages
+  resetAll()
+  if (fname.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    fnameError.textContent = ''; // Reset the content of the message
+    fnameError.className = 'error'; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError(fname, fnameError);
+  }
+} );
+
+fname.addEventListener('input', function (event) {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+  if (fname.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    fnameError.textContent = ''; // Reset the content of the message
+    fnameError.className = 'error'; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError(fname, fnameError);
+  }
+});
+
+// setting restrictions on LASTNAME input, same as above
+const lname = document.getElementById('lastname_input');
+const lnameError = document.querySelector('#lastname_input + span.error');
+
+lname.addEventListener('focus', function(event) { 
+  resetAll()
+  if (lname.validity.valid) {
+    lnameError.textContent = '';
+    lnameError.className = 'error';
+  } else {
+    showError(lname, lnameError);
+  } } );
+lname.addEventListener('input', function (event) {
+  if (lname.validity.valid) {
+    lnameError.textContent = '';
+    lnameError.className = 'error';
+  } else {
+    showError(lname, lnameError);
+  }
+});
+
+// setting restrictions on EMAIL input, same as above
+const email = document.getElementById('email_input');
+const emailError = document.querySelector('#email_input + span.error');
+
+email.addEventListener('focus', function(event) { 
+  resetAll()
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    emailError.className = 'error';
+  } else {
+    showError(email, emailError);
+  } } );
+
+email.addEventListener('input', function (event) {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    emailError.className = 'error';
+  } else {
+    showError(email, emailError);
+  }
+});
+
+// setting restrictions on PASSWORD input, same as above
+const pass = document.getElementById('pass_input');
+const passError = document.querySelector('#pass_input + span.error');
+
+pass.addEventListener('focus', function(event) { 
+  resetAll()
+  if ((pass.validity.valid) && (/\d/.test(pass.value)) && (/\D/.test(pass.value)) ) {
+    passError.textContent = '';
+    passError.className = 'error';
+  }
+  else {
+    pass.validity.valid = false;
+    showError(pass, passError, minl=8, maxl=255, is_password=true);
+  } } );
+
+pass.addEventListener('input', function (event) {
+  if ((pass.validity.valid) && (/\d/.test(pass.value)) && (/\D/.test(pass.value)) ) {
+    passError.textContent = '';
+    passError.className = 'error';
+  }
+  else {
+    pass.validity.valid = false;
+    showError(pass, passError, minl=8, maxl=255, is_password=true);
+  }
+});
+
+// setting restrictions on ADDRESS input, same as above
+const adrr = document.getElementById('address_input');
+const adrrError = document.querySelector('#address_input + span.error');
+
+adrr.addEventListener('focus', function(event) { 
+  resetAll()
+  if (adrr.validity.valid) {
+    adrrError.textContent = '';
+    adrrError.className = 'error';
+  } else {
+    showError(adrr, adrrError, maxl=10);
+  } } );
+  
+adrr.addEventListener('input', function (event) {
+  if (adrr.validity.valid) {
+    adrrError.textContent = '';
+    adrrError.className = 'error';
+  } else {
+    showError(adrr, adrrError, maxl=10);
+  }
+});
+
+const form  = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', function (event) {
+
+  fname.value = fname.value.trim()
+  lname.value = lname.value.trim()
+  email.value = email.value.trim()
+
+  if ((!fname.validity.valid) || (!lname.validity.valid) || (!email.validity.valid) ||
+     (!pass.validity.valid) || (!addr.validity.valid)) {
+    window.alert("SHIT")
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+  else { // form is valid
+
+  }
+});
+
+function showError(entity, entityERROR, minl=0, maxl=255, is_password=false) {
+    if(entity.validity.valueMissing) {
+      // If the field is empty,
+      // display the following error message.
+      entityERROR.textContent = 'این فیلد نباید خالی باشد';
+    } else if(entity.validity.typeMismatch) {
+      // If the field doesn't contain an name address,
+      // display the following error message.
+      entityERROR.textContent = 'یک مقدار معتبر وارد نمایید';
+    } else if(entity.validity.tooLong || entity.value.length>maxl) {
+      // If the data is too long,
+      // display the following error message.
+      entityERROR.textContent = `حداکثر تعداد کاراکتر قابل قبول ${ maxl } است`;
+    }
+    else if(entity.validity.tooShort || entity.value.length<minl) {
+        // If the data is too short,
+        // display the following error message.
+        entityERROR.textContent = `حداقل تعداد کاراکتر قابل قبول ${ minl } است`;
+      }
+    else if(is_password)
+    {
+        entityERROR.textContent = 'رمز عبور باید شامل لااقل یک عدد و یک حرف باشد';
+    }
+  
+    // Set the styling appropriately
+    entityERROR.className = 'error active';
+  }
+function resetAll() {
+  fnameError.textContent = '';
+  lnameError.textContent = '';
+  emailError.textContent = '';
+  passError.textContent = '';
+  adrrError.textContent = '';
+}
