fc01ca88e9625b090204643ea6a5d0785bd1eadb
manaatarod <mana.atarod@gmail.com>
2021-07-20 02:54:19 +0430

    go to index and scroll to products

---
+++ Script/menu.js
@@ -0,0 +1,27 @@
+function findPos(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    console.log(curtop)
+    return [curtop];
+    }
+}
+
+function goToIndexAndScroll(e) {
+    e.preventDefault();
+    var pathname = window.location.pathname.split("/").pop();
+    console.log(pathname);
+    if (pathname != 'index.html') {
+        console.log("not in index")
+        window.location.href = "index.html#product-list-div";
+    }
+    /*location.href = "#product-list-div";*/
+    console.log("continuing")
+    /*window.focus();*/
+    window.scrollTo(0,findPos(document.getElementById("product-list-div")));
+}
+
+
+document.getElementById("menu-item-products").addEventListener("click", goToIndexAndScroll);
